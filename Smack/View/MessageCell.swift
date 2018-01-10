a48fc6f17ade77f6553bb7a417be49f760338d9c
ahmeddsoky <ahmed200526@gmail.com>
2018-01-10 23:45:01 +0200

    finish

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -28,6 +28,22 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImage.image = UIImage(named:message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        // format timeStamp
+        // unwrap data
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("z"))
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:m a"
+        
+        if let finalDate = chatDate{
+            let finalDate = newFormatter.string(from: finalDate)
+            
+            timeStampLbl.text = finalDate
+        }
     }
     
     
